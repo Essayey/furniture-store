5d4d8f0b8a4411d77cfb41628d7d5aa59f1ab099
Dania <danila.lusuy@gmail.com>
2024-03-17 21:20:57 +0300

    feat: added header

---
+++ frontend/src/widgets/header/ui/header.tsx
@@ -0,0 +1,21 @@
+import { Routes } from "@/shared/consts";
+import { Button } from "@/shared/ui";
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <header className=" bg-accentPrimary  py-6">
+      <div className=" flex  ">
+        <Button asChild>
+          <Link to={Routes.about}> About page</Link>
+        </Button>
+        <Button asChild>
+          <Link to={Routes.cart}> Cart page</Link>
+        </Button>
+        <Button asChild>
+          <Link to={Routes.main}> Main page</Link>
+        </Button>
+      </div>
+    </header>
+  );
+};
