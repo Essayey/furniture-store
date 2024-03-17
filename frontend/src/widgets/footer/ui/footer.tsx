0611170d0d43d557bd1071f3f05ddd27cf7bd99d
Dania <danila.lusuy@gmail.com>
2024-03-17 21:20:50 +0300

    feat: added footer

---
+++ frontend/src/widgets/footer/ui/footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className=" bg-accentPrimary  py-6">
+      <div className="container mx-auto flex justify-center space-x-44">
+        <div className="text-textSecondary">Text</div>
+        <div className="text-textSecondary">Text</div>
+        <div className="text-textSecondary">Text</div>
+      </div>
+    </footer>
+  );
+};
