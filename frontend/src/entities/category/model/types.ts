a5895d9b31bf73b7db34261ad504a0364b776845
SenezAr <danila.lusuy@gmail.com>
2024-03-18 14:21:49 +0300

    feat: added Category type

--- frontend/src/entities/category/model/types.ts
+++ frontend/src/entities/category/model/types.ts
@@ -1,6 +1,15 @@
+export type Property = {
+  id:number,
+  name:string,
+  options: string[],
+  description:string,
+  optional:boolean
+}
 export type Category = {
   id: number;
   parentId: number;
   name: string;
   description: string;
+  childCategories: Category[];
+  properties:
 };
