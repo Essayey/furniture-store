4edcfc12b7c73e0567532685c9f2482b14d82138
SenezAr <danila.lusuy@gmail.com>
2024-03-18 14:23:19 +0300

    feat: change styles

--- frontend/src/widgets/header/ui/header.tsx
+++ frontend/src/widgets/header/ui/header.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const Header = () => {
   return (
     <header className=" bg-accentPrimary  py-6">
-      <div className=" flex  ">
+      <div className=" flex gap-5 justify-center ">
         <Button asChild>
           <Link to={Routes.about}> About page</Link>
         </Button>
