f6b61bffa165574afb4cb866bd19717cd8181fdc
Dania <danila.lusuy@gmail.com>
2024-03-21 12:26:33 +0300

    feat: add endpoints, getAll and getById

--- frontend/src/entities/category/api/categoryApi.ts
+++ frontend/src/entities/category/api/categoryApi.ts
@@ -1,12 +1,25 @@
 import { baseApi } from "@/shared/api";
-import { getAllCategoriesDtoResponse } from "./categoryDto";
+import {
+  getAllCategoriesDtoResponse,
+  getCategoriesByIdDtoResponse,
+  getCategoriesByIdDtoRequest,
+} from "./categoryDto";
 
 export const categoryApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
     getAllCategories: build.query<getAllCategoriesDtoResponse, void>({
       query: () => ({
-        url: `/ca`,
+        url: `/categories`,
+        method: "GET",
+      }),
+    }),
+    getCategoriesById: build.query<getCategoriesByIdDtoResponse, getCategoriesByIdDtoRequest>({
+      query: ({ id }) => ({
+        url: `/categories/${id}`,
+        method: "GET",
       }),
     }),
   }),
 });
+export const { useGetAllCategoriesQuery, useGetCategoriesByIdQuery, useLazyGetAllCategoriesQuery } =
+  categoryApi;
