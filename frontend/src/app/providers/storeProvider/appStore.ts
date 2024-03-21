2df7f10f06959b62dc1c16d76cb7332e75502d3f
Dania <danila.lusuy@gmail.com>
2024-03-21 12:26:00 +0300

    feat: add devTools

--- frontend/src/app/providers/storeProvider/appStore.ts
+++ frontend/src/app/providers/storeProvider/appStore.ts
@@ -5,10 +5,12 @@ import { baseApi } from "@/shared/api";
 export const createStore = () => {
   const store = configureStore({
     reducer: rootReducer,
+    devTools: true,
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(baseApi.middleware),
   });
   return store;
 };
 export const appStore = createStore();
+
 export type RootState = ReturnType<typeof appStore.getState>;
 export type AppDispatch = typeof appStore.dispatch;
