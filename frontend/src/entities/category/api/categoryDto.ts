245e93bd32d3960016eba245e16f103949c12260
Dania <danila.lusuy@gmail.com>
2024-03-21 12:26:40 +0300

    feat: add dto

--- frontend/src/entities/category/api/categoryDto.ts
+++ frontend/src/entities/category/api/categoryDto.ts
@@ -1,3 +1,6 @@
 import { Category } from "../model/types";
 
 export type getAllCategoriesDtoResponse = Category[];
+
+export type getCategoriesByIdDtoResponse = Category;
+export type getCategoriesByIdDtoRequest = { id: string };
