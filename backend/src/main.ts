125c44f85d4381ab12f172887b2ac9faac01bdb3
Dania <danila.lusuy@gmail.com>
2024-03-21 12:25:52 +0300

    feat: add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,22 +1,22 @@
-import { NestFactory } from "@nestjs/core"
-import { AppModule } from "./app.module"
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function start() {
-    const PORT = process.env.PORT || 5000
-    const app = await NestFactory.create(AppModule)
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: "*" });
+  const config = new DocumentBuilder()
+    .setTitle("Furniture Store")
+    .setDescription("API")
+    .setVersion("1.0.0")
+    .addTag("Diplom")
+    .build();
 
-    const config = new DocumentBuilder()
-        .setTitle('Furniture Store')
-        .setDescription('API')
-        .setVersion('1.0.0')
-        .addTag('Diplom')
-        .build()
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("/api/docs", app, document);
 
-    const document = SwaggerModule.createDocument(app, config)
-    SwaggerModule.setup('/api/docs', app, document)
-
-    await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
 
-start()
+start();
