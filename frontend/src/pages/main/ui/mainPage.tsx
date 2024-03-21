4ca339d15315b93dce78437a3dc7c497e6f30aa3
Dania <danila.lusuy@gmail.com>
2024-03-21 12:27:12 +0300

    feat: add fetch data

--- frontend/src/pages/main/ui/mainPage.tsx
+++ frontend/src/pages/main/ui/mainPage.tsx
@@ -1,3 +1,11 @@
-export const MainPage = () => {
-  return <div></div>;
-};
+import { useGetAllCategoriesQuery } from "@/entities/category";
+import { memo } from "react";
+
+export const MainPage = memo(() => {
+  const { data, isLoading } = useGetAllCategoriesQuery();
+  console.log();
+  if (isLoading) return null;
+  if (data) console.log(data);
+  console.log(data);
+  return <div>asdfasdf</div>;
+});
