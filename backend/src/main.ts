591bd09d22087b4ce03d2696f52236f216784e9a
SenezAr <danila.lusuy@gmail.com>
2024-03-22 17:40:23 +0300

    feat: added cors rules

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,10 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: "*" });
+  app.enableCors({
+    origin: "*",
+    methods: ["GET", "PATCH", "PUT", "POST", "DELETE"],
+  });
   const config = new DocumentBuilder()
     .setTitle("Furniture Store")
     .setDescription("API")
