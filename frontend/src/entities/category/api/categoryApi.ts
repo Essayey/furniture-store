f878283772b42c0f49a2396a6373ad92c2857a07
Dania <danila.lusuy@gmail.com>
2024-03-23 17:56:42 +0300

    feat: add categoryApi endpoints

--- frontend/src/entities/category/api/categoryApi.ts
+++ frontend/src/entities/category/api/categoryApi.ts
@@ -1,45 +1,83 @@
 import { baseApi } from "@/shared/api";
+
 import {
-  allCategoriesDtoResponse,
-  categoriesByIdDtoRequest,
-  categoriesByIdDtoResponse,
-  createCategoriesDtoRequest,
-  pathCategoryByIdDtoRequest,
-  pathCategoryByIdDtoResponse,
+  AddPropertyToCategoryDtoRequest,
+  AddPropertyToCategoryDtoResponse,
+  AllCategoriesDtoRequset,
+  AllCategoriesDtoResponse,
+  CategoriesByIdDtoRequest,
+  CategoriesByIdDtoResponse,
+  CreateCategoriesDtoRequest,
+  CreateCategoriesDtoResponse,
+  DeleteCategoryByIdDtoRequest,
+  DeleteCategoryByIdDtoResponse,
+  DeletePropertyFromCategoryDtoRequest,
+  DeletePropertyFromCategoryDtoResponse,
+  PathCategoryByIdDtoRequest,
+  PathCategoryByIdDtoResponse,
 } from "./categoryDto";
-import { CATEGORIES_TAG } from "@/shared/api/tags";
 
 export const categoryApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
-    allCategories: build.query<allCategoriesDtoResponse, void>({
+    allCategories: build.query<AllCategoriesDtoResponse, AllCategoriesDtoRequset>({
       query: () => ({
         url: `/categories`,
         method: "GET",
       }),
-      providesTags: [CATEGORIES_TAG],
+      providesTags: ["CATEGORIES_TAG"],
     }),
 
-    categoriesById: build.query<categoriesByIdDtoResponse, categoriesByIdDtoRequest>({
+    categoriesById: build.query<CategoriesByIdDtoResponse, CategoriesByIdDtoRequest>({
       query: ({ id }) => ({
         url: `/categories/${id}`,
         method: "GET",
       }),
     }),
-    createCategories: build.mutation<void, createCategoriesDtoRequest>({
+    createCategories: build.mutation<CreateCategoriesDtoResponse, CreateCategoriesDtoRequest>({
       query: ({ description, name, parentId }) => ({
         url: "/categories",
         method: "POST",
         body: { description, name, parentId },
       }),
-      invalidatesTags: [CATEGORIES_TAG],
+      invalidatesTags: ["CATEGORIES_TAG"],
     }),
-    pathCategoryById: build.mutation<pathCategoryByIdDtoResponse, pathCategoryByIdDtoRequest>({
+    pathCategoryById: build.mutation<PathCategoryByIdDtoResponse, PathCategoryByIdDtoRequest>({
       query: ({ id, description, name, parentId }) => ({
         url: `/categories/${id}`,
         method: "PATCH",
         body: { description, name, parentId },
       }),
-      invalidatesTags: [CATEGORIES_TAG],
+      invalidatesTags: ["CATEGORIES_TAG"],
+    }),
+    deleteCategoryById: build.mutation<DeleteCategoryByIdDtoResponse, DeleteCategoryByIdDtoRequest>(
+      {
+        query: ({ id }) => ({
+          url: `/categories/${id}`,
+          method: "DELETE",
+        }),
+        invalidatesTags: ["CATEGORIES_TAG"],
+      },
+    ),
+    addPropertyToCategory: build.mutation<
+      AddPropertyToCategoryDtoResponse,
+      AddPropertyToCategoryDtoRequest
+    >({
+      query: ({ categoryId, propertyId }) => ({
+        url: `/categories/addPropertyToCategory/${categoryId}/${propertyId}`,
+        method: "PUT",
+        body: { categoryId, propertyId },
+      }),
+      invalidatesTags: ["CATEGORIES_TAG"],
+    }),
+    removePropertyFromCategory: build.mutation<
+      DeletePropertyFromCategoryDtoResponse,
+      DeletePropertyFromCategoryDtoRequest
+    >({
+      query: ({ categoryId, propertyId }) => ({
+        url: `/categories/removePropertyFromCategory/${categoryId}/${propertyId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["CATEGORIES_TAG"],
     }),
   }),
 });
@@ -48,4 +86,7 @@ export const {
   useCategoriesByIdQuery,
   useCreateCategoriesMutation,
   usePathCategoryByIdMutation,
+  useDeleteCategoryByIdMutation,
+  useAddPropertyToCategoryMutation,
+  useRemovePropertyFromCategoryMutation,
 } = categoryApi;
