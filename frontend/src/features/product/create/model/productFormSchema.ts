fec893abd5c6422c4b6d5a7a14746254c5e0d5be
Dania <danila.lusuy@gmail.com>
2024-03-24 19:31:46 +0300

    feat: add productFormSchema

---
+++ frontend/src/features/product/create/model/productFormSchema.ts
@@ -0,0 +1,24 @@
+import { ACCEPTED_IMAGE_TYPES, MAX_FILE_SIZE } from "@/shared/consts";
+import { z } from "zod";
+
+export const productFormSchema = z.object({
+  name: z
+    .string({ required_error: "name field requierd!" })
+    .min(2, { message: "name must be at least 2 characters" }),
+  description: z.string({ required_error: "description field requierd!" }),
+  image: z
+    .any()
+    .refine((file) => file?.size <= MAX_FILE_SIZE, `Max image size is 5MB.`)
+    .refine(
+      (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+      "Only .jpg, .jpeg, .png and .webp formats are supported.",
+    ),
+  categoryId: z
+    .number({ required_error: "categoryId field requierd!" })
+    .positive({ message: "categoryId cannot be negative " })
+    .nullable(),
+  properties: z.array(z.object({ propertyId: z.number().nonnegative(), value: z.string() })),
+  amount: z.number().nonnegative(),
+  price: z.number().nonnegative(),
+});
+export type ProductFormSchema = z.infer<typeof productFormSchema>;
