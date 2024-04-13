e336f334521cd12342a154db863e9eb1e4cd9cf9
ArtemSarafanov <lucky84231@gmail.com>
2024-04-13 14:40:28 +0300

    card

--- frontend/src/pages/main/ui/mainPage.tsx
+++ frontend/src/pages/main/ui/mainPage.tsx
@@ -1,17 +1,21 @@
-import {
-  useAllCategoriesQuery,
-  useCreateCategoryMutation,
-  usePathCategoryByIdMutation,
-} from "@/entities/category";
-import { memo, useEffect } from "react";
+import { ProductCard, useAllProductsQuery } from "@/entities/product";
+import { memo } from "react";
 
 export const MainPage = memo(() => {
-  const { data, isLoading } = useAllCategoriesQuery();
-  const [createCategory] = useCreateCategoryMutation();
-  const [updateCategory] = usePathCategoryByIdMutation();
+  const { data: products, isLoading } = useAllProductsQuery()
 
-  console.log(data);
+  if (isLoading) return null
+
   return (
-    <div>{data?.map((category) => category?.childCategories.map((category) => category.name))}</div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {products?.map(product =>
+        <ProductCard
+          description={product.description}
+          img={`http://localhost:7000/${product.img}`}
+          name={product.name}
+          price={String(product.price)}
+          onAddToCart={() => { }}
+        />)}
+    </div>
   );
 });
